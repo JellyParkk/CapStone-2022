f77ec7853294be3e61bfcd12e3e14a1cb12931b0
june0556 <71110881+june0556@users.noreply.github.com>
2022-08-31 09:41:11 +0900

    Revert "[기능수정] 코드 정리 및 누락된 기능 추가"

--- Capstone/example/src/main/java/com/r0adkll/slidr/example/ViewerActivity.java
+++ Capstone/example/src/main/java/com/r0adkll/slidr/example/ViewerActivity.java
@@ -52,6 +52,7 @@ public class ViewerActivity extends AppCompatActivity {
     String Filename;
     String path0;
     String filecontents;
+    String titlename;
 
 
     private TextFile mTextFile;
@@ -66,6 +67,8 @@ public class ViewerActivity extends AppCompatActivity {
         int primary = getResources().getColor(R.color.primaryDark);
         int secondary = getResources().getColor(R.color.red_500);
 
+        int numPositions = SlidrPosition.values().length;
+        SlidrPosition position = SlidrPosition.values()[Utils.getRandom().nextInt(numPositions)];
 
         title = findViewById(R.id.title);
         txtRead =  findViewById(R.id.description);
@@ -81,6 +84,7 @@ public class ViewerActivity extends AppCompatActivity {
 //                .edge(true)
                 .touchSize(SizeUtils.dpToPx(this, 32))
                 .build();
+        //Slidr.attach(this,mConfig);
 
         setSupportActionBar(mToolbar);
         getSupportActionBar().setTitle("");
@@ -88,7 +92,12 @@ public class ViewerActivity extends AppCompatActivity {
         mTextFile = getIntent().getParcelableExtra(EXTRA_FILE);
         if(savedInstanceState != null) {
             mTextFile = savedInstanceState.getParcelable(EXTRA_FILE);
+            Log.d("tlqkf3", String.valueOf(mTextFile));
         }
+
+        Log.d("tlqkf2", String.valueOf(mTextFile));
+        Log.d("tlqkf2", String.valueOf(mTextFile.text_title));
+        //mTitle.setText(mTextFile.text_title);
         String[] temp = mTextFile.text_title.split("\\.");
         String title ="";
         for(int i=0;i<temp.length-1;i++) {
@@ -97,11 +106,17 @@ public class ViewerActivity extends AppCompatActivity {
                 title = title.concat(".");
             }
         }
+        Log.d("tlqkfxkdlxmf",title);
         mTitle.setText(title);
         mDescription.setText(mTextFile.text_context);
         Filename = mTextFile.text_title;
+        //path0 = mTextFile.text_date;
         path0 = Environment.getExternalStorageDirectory().toString()+"/Download/TestDir";
         filecontents = mTextFile.text_context;
+        titlename = mTextFile.text_title;
+        Log.d("tlqkf2", Filename);
+        Log.d("tlqkf2", path0);
+        Log.d("tlqkf2", filecontents);
     }
 
 
@@ -123,6 +138,8 @@ public class ViewerActivity extends AppCompatActivity {
     public class DeleteFragment extends DialogFragment {
         @Override
         public Dialog onCreateDialog(Bundle savedInstanceState) {
+            Log.d("TESTTAG","dialog");
+            // Use the Builder class for convenient dialog construction
             AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
             builder.setMessage("정말 삭제하시겠습니까?")
                     .setPositiveButton("삭제", new DialogInterface.OnClickListener() {
@@ -132,8 +149,10 @@ public class ViewerActivity extends AppCompatActivity {
                     })
                     .setNegativeButton("취소", new DialogInterface.OnClickListener() {
                         public void onClick(DialogInterface dialog, int id) {
+                            // User cancelled the dialog
                         }
                     });
+            // Create the AlertDialog object and return it
             return builder.create();
         }
     }
@@ -143,6 +162,9 @@ public class ViewerActivity extends AppCompatActivity {
             d = new File(path0, (Filename));
             f = new File(path0, (newfilename + ".txt"));
             int numberingName = 1;
+
+            Log.d("TESTTAG",d.getName());
+            Log.d("TESTTAG",f.getName());
             BufferedWriter file = new BufferedWriter(new FileWriter(f));
             String data = filecontents;
             file.write(data);
@@ -155,12 +177,14 @@ public class ViewerActivity extends AppCompatActivity {
 
         } catch (Exception e) {
             e.printStackTrace();
+            Log.d("testTag","save fail");
         }
         MediaScanner ms = MediaScanner.newInstance(ViewerActivity.this);
         try {
             ms.mediaScanning(path0);
         } catch (Exception e) {
             e.printStackTrace();
+            Log.d("MediaScan", "ERROR" + e);
         }
         finally {
 
@@ -179,6 +203,7 @@ public class ViewerActivity extends AppCompatActivity {
                 .setPositiveButton("수정", new DialogInterface.OnClickListener() {
                     public void onClick(DialogInterface dialog, int id) {
                         et1 = (EditText) v_d.findViewById(R.id.et_title);
+                        Log.d("TESTTAG", et1.getText().toString());
                         File tem = new File(path0, (et1.getText().toString() + ".txt"));
                         if(et1.getText().toString().equals(""))
                         {
@@ -193,10 +218,14 @@ public class ViewerActivity extends AppCompatActivity {
                             title.setText(et1.getText().toString());
                             save(title.getText().toString());
                         }
+
+                        Log.d("TESTTAG","onclick");
+
                     }
                 })
                 .setNegativeButton("취소", new DialogInterface.OnClickListener() {
                     public void onClick(DialogInterface dialog, int id) {
+                        // User cancelled the dialog
                     }
                 });
         changedialog.show();
@@ -216,8 +245,10 @@ public class ViewerActivity extends AppCompatActivity {
                 })
                 .setNegativeButton("취소", new DialogInterface.OnClickListener() {
                     public void onClick(DialogInterface dialog, int id) {
+                        // User cancelled the dialog
                     }
                 });
+        // Create the AlertDialog object and return it
         AlertDialog deletedialog = builder.create();
         deletedialog.show();
 
@@ -227,4 +258,20 @@ public class ViewerActivity extends AppCompatActivity {
         delete();
     }
 
+    private void checkPermission() {
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if(checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED
+                    || checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+                if(shouldShowRequestPermissionRationale(Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+
+                }
+
+                requestPermissions(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE,Manifest.permission.READ_EXTERNAL_STORAGE}, 2);  //마지막 인자는 체크해야될 권한 갯수
+            } else {
+
+            }
+        }
+
+    }
 }
